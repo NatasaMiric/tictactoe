f5fd815b30541476775744d5e466c4421886932e
NatasaMiric <natasa.miric94@gmail.com>
2022-11-19 16:21:45 +0000

    Create display_board function to display the game board

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to Tic Tac Toe Game!")
+
+board = {
+    '1': ' ', '2': ' ', '3': ' ', 
+    '4': ' ', '5': ' ', '6': ' ',
+    '7': ' ', '8': ' ', '9': ' '
+}
+
+
+def display_board():
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+
+
+
+display_board()
