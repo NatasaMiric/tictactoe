7e5665c20eedfff6d834f2aae1eb3ff020c707e7
NatasaMiric <natasa.miric94@gmail.com>
2022-11-22 15:03:50 +0000

    Add statement for handling invalid input

--- run.py
+++ run.py
@@ -2,24 +2,59 @@ import random
 import os
 
 board = {
-    '1': ' ', '2': ' ', '3': ' ',
-    '4': ' ', '5': ' ', '6': ' ',
-    '7': ' ', '8': ' ', '9': ' '
+        '1': ' ', '2': ' ', '3': ' ',
+        '4': ' ', '5': ' ', '6': ' ',
+        '7': ' ', '8': ' ', '9': ' '
     }
 
-WINNER = None
-CURRENT_PLAYER = "X"
+turn = 0  # Keeps track whos turn it is
 
 
-def display_board():
+def display_board(board):
     """
     Displays the game board
     """
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
-    print('-----')
+    print('-+-+-')
     print(board['4'] + '|' + board['5'] + '|' + board['6'])
-    print('-----')
+    print('-+-+-')
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
+
+
+def run_game():
+    """
+    Function that runs the game until all the cells in the board are occupied
+    """
+    while not isboardfull():
+        display_board(board)
+        print("\nPlease enter a number (1-9) or q to quit the game: ")
+        
+        try: 
+            user_input = input()
+            if user_input.lower() == 'q':
+                print("Thank you for playing!")
+                break
+            if board[user_input] == ' ':            
+                board[user_input] = 'X'
+                generate_computer_input()
+                # Clears the console
+                os.system('cls' if os.name == 'nt' else 'clear')              
+            else:
+                print("\nThat place is already taken.Choose another spot.\n")
+                continue
+        except KeyError:
+            print('Invalid input.Please try again') 
+ 
+   
+def isboardfull():
+    """
+    Checks if the board has empty locations
+    """
+    values = board.values()
+    for value in values:
+        if value == ' ':
+            return False
+    return True
 
 
 def generate_computer_input():
@@ -29,89 +64,42 @@ def generate_computer_input():
     random_move = list(board.keys())
     computer_input = random.choice(random_move)
     if not isboardfull():
-        if board[computer_input] != "X" and board[computer_input] != "O":
-            board[computer_input] = "O"
+        if not board[computer_input] in {'X', 'O'}:
+            board[computer_input] = 'O'
         else:
             generate_computer_input()
 
 
-def isboardfull():
-    """
-    Checks if the board has empty locations
-    """
-    values = board.values()
-    for value in values:
-        if value != "X" and value != "O":
-            return False
-    return True
+# def check_win():
+
+#     if check_diagonal() or check_horizontal() or check_vertical():
+#         return True
 
 
-def run_game():
-    """
-    Function that runs the game until all the cells in the board are occupied
-    """
-    while not isboardfull():
-        display_board()
-        print("\nPlease enter a number (1-9): ")
-        user_input = input()
-        if board[user_input] == ' ':
-            board[user_input] = "X"
-            generate_computer_input()
-            check_win()
-            clear_screen()
-        else:
-            print(
-                "\nThat place is already filled."
-                "Choose another location.\n"
-                )
-            continue
-    display_board()
+# def check_horizontal():
+#     if (board[1] == board[2] == board[3] and board[1] != "-") or\
+#         (board[4] == board[5] == board[6] and board[4] != "-") or\
+#             (board[7] == board[8] == board[9] and board[7] != "-"):
+#         return True
 
 
-def check_win():
-    if check_diagonal() or check_horizontal() or\
-         check_vertical():
-        print(f"The winner is {WINNER}")
+# def check_vertical():
+#     if (board[1] == board[4] == board[7] and board[1] != "-") or\
+#         (board[2] == board[5] == board[8] and board[2] != "-") or\
+#             (board[3] == board[6] == board[9] and board[3] != "-"):
+#         return True
 
 
-def check_horizontal():
-    global WINNER
-    if (board['1'] == board['2'] == board['3'] and board['1'] != "-") or\
-        (board['4'] == board['5'] == board['6'] and board['4'] != "-") or\
-            (board['7'] == board['8'] == board['9'] and board['7'] != "-"):
-        WINNER = CURRENT_PLAYER
-        return True
-
-
-def check_vertical():
-    global WINNER
-    if (board['1'] == board['4'] == board['7'] and board['1'] != "-") or\
-        (board['2'] == board['5'] == board['8'] and board['2'] != "-") or\
-            (board['3'] == board['6'] == board['9'] and board['3'] != "-"):
-        WINNER = CURRENT_PLAYER
-        return True
-
-
-def check_diagonal():
-    global WINNER
-    if (board['1'] == board['5'] == board['9'] and board['1'] != "-") or\
-       (board['3'] == board['5'] == board['7'] and board['3'] != "-"):
-        WINNER = CURRENT_PLAYER
-        return True
+# def check_diagonal():
+#     if (board[1] == board[5] == board[9] and board[1] != "-") or\
+#        (board[3] == board[5] == board[7] and board[3] != "-"):
+#         return True
 
 
 # def check_tie():
 #     if ' ' not in board:
-#         display_board()
+#         display_board(board)
 #         print("Its a tie")
-
-
-def switch_player():
-    global CURRENT_PLAYER
-    if CURRENT_PLAYER == "X":
-        CURRENT_PLAYER = "O"
-    else:
-        CURRENT_PLAYER = "X"
 
 
 def display_instructions():
@@ -134,30 +122,13 @@ def display_instructions():
     )
 
 
-def clear_screen(numlines=100):
-    """
-    Clears the console to simplify UX and clear visual clutter.
-    numlines is an optional argument used only as a fall-back.
-    """
-    if os.name == "posix":
-        # for OS => Unix / Linux / MacOS / BSD / etc
-        os.system('clear')
-    elif os.name in ("nt", "dos", "ce"):
-        #  for OS => DOS / Windows
-        os.system('CLS')
-    else:
-        # Fallback for other operating systems.
-        print('\n' * numlines)
-
-
 def main():
     """
     Main function
     """
     display_instructions()
     run_game()
-    check_win()
-    switch_player()
+    
 
 
 print("Welcome to Tic Tac Toe Game!")
